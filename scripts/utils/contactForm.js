411455e840ced6201fa1e1952512a0948c9ace37
Veizh <maximedurville95300@gmail.com>
2023-10-24 13:41:00 +0200

    form finished

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,18 +1,28 @@
 import { enabledMainAria,disabledMainAria } from "./navKeyboard.js";
 let buttonOpen = document.querySelector('.modal_opening ')
+let btn = document.querySelector('.contact_button_submit')
+
 let buttonClose = document.querySelector('.close_btn')
-console.log(buttonOpen);
+
+
+btn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    let inputs = document.querySelectorAll('input')
+    inputs.forEach(e=>console.log(e.name,"=>",e.value))
+
+}
+    
+    )
 buttonOpen.addEventListener('click',()=>{
-    console.log("test");
     displayModal()
 })
 buttonClose.addEventListener('click',()=>{
-    console.log("test");
     closeModal()
 })
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+
     disabledMainAria()
 }
 
